9256fed9c3c4863747fe23d1fcbfef9f5363498d
Zhou Fang <fang1534@stthomas.edu>
2020-11-26 20:15:37 -0600

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        //Will hit this point when users go the below address.
+        //api/users/3 <--Last input will be the user id.
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+           return await _context.Users.FindAsync(id);
+        }
+    }
+}
